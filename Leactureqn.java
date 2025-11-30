28b31ee68f1b9baaeab227aaa13b37a04522a915
lakii <luckymaharjan2007@gmail.com>
2025-11-30 22:23:04 +0545

    lecture

---
+++ Leactureqn.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class Leactureqn here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Leactureqn
+{
+    public static void main(String[] args)
+    {
+        for (int i = 1;i<=4;i++)
+        {
+            for(int j=1;j<=4;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
