9398e5c8f90b64484b2cafde1ebfb164cb319b99
lakiiii <luckymaharjan2007@gmail.com>
2025-11-30 21:04:43 +0545

    q11

--- q11.java
+++ q11.java
@@ -20,22 +20,22 @@ public class q11
         }
         else if (income>500000 && income<=700000)
         {
-            double tax= 0.01*500000 + (700000-income)*0.10d;
+            double tax= 0.01*500000 + (income-500000)*0.10d;
             System.out.println("The total tax is :Rs"+tax);
         }
          else if (income>700000 && income<=1000000)
         {
-            double tax= 0.01*500000 + 0.10*200000d + (1000000-income)*0.20d;
+            double tax= 0.01*500000 + 0.10*200000d + (income-700000)*0.20d;
             System.out.println("The total tax is :Rs"+tax);
         }
         else if (income>1000000 && income<=2000000)
         {
-            double tax= 0.01*500000 + 0.10*200000d + 0.20*300000 +(2000000-income)*0.20d;
+            double tax= 0.01*500000 + 0.10*200000d + 0.20*300000 +(income-1000000)*0.20d;
             System.out.println("The total tax is :Rs"+tax);
         }
         else if (income>2000000 && income<=5000000)
         {
-            double tax= 0.01*500000 + 0.10*200000d + 0.20*300000 +0.30*1000000+(5000000-income)*0.20d;
+            double tax= 0.01*500000 + 0.10*200000d + 0.20*300000 +0.30*1000000+(income-2000000)*0.20d;
             System.out.println("The total tax is :Rs"+tax);
         }
         else
